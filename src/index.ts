adb6688933f89da0846d7b78f855dc05f431a14e
Thomas Zerr <thomas.zerr@gmail.com>
2021-06-05 18:15:43 +0200

    -added: result presentation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hallo Welt");
+import { SudokuImpl } from "./SudokuImpl";
+
+const sudokuToSolve = [
+    9,0,0, 0,0,0, 0,6,0,
+    0,0,0, 0,8,0, 1,9,0,
+    0,0,7, 0,1,0, 0,0,0,
+
+    0,0,0, 0,0,6, 0,7,3,
+    0,0,0, 3,0,9, 0,0,0,
+    3,4,5, 0,0,0, 0,0,0,
+
+    4,0,0, 0,0,2, 6,8,0,
+    0,0,2, 5,0,0, 0,0,0,
+    0,3,0, 7,0,0, 0,0,0
+];
+
+const sudokuField = new SudokuImpl(sudokuToSolve);
+
+console.log(`Field to solve:\n${sudokuField.toString()}`);
+console.log("------------------------------------");
+
+console.time("Solved field in: ");
+let solvedSudokuField = sudokuField.solveAndGetResult();
+console.timeEnd("Solved field in: ");
+
+if(solvedSudokuField != null) {
+    console.log(`Solved field:\n${solvedSudokuField.toString()}`);
+} else {
+    console.error("Could not solve field");
+}
